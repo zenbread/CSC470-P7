8525bc26f3fa826625327a92dfe42cf79c4c3fa6
nick <nmcamp87@gmail.com>
2021-11-26 12:06:12 -0500

    Added requirements class

---
+++ Requirement.cs
@@ -0,0 +1,10 @@
+﻿namespace P7
+{
+    public class Requirement
+    {
+        public int Id;
+        public int ProjectId;
+        public int FeatureId;
+        public string Statement;
+    }
+}
