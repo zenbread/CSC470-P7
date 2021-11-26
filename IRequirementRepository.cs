b6077122625c00114e6723686ea3f85a6268002f
nick <nmcamp87@gmail.com>
2021-11-26 12:06:26 -0500

    Added interface for repo

---
+++ IRequirementRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace P7
+{
+    interface IRequirementRepository
+    {
+        string Add(Requirement requirement);
+        List<Requirement> GetAll(int ProjectId);
+        string Modify(Requirement requirement);
+        Requirement GetRequirementById(int requirementId);
+        int CountByFeatureId(int featureId);
+        void RemoveByFeatureId(int featureId);
+    }
+}
